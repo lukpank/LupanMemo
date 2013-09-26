4ec51c9009fda1cc1724fb6a728c16283eb576fa
Łukasz Pankowski <lukpank@o2.pl>
2013-09-26 23:13:17 +0200

    add about dialog

--- src/com/example/lupan/memo/MainActivity.java
+++ src/com/example/lupan/memo/MainActivity.java
@@ -5,11 +5,16 @@ import com.example.lupan.memo.R;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.text.Html;
 import android.util.Log;
 
 
@@ -53,6 +58,19 @@ public class MainActivity extends Activity implements OnSharedPreferenceChangeLi
 		return true;
 	}
 
+	public void about() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+		LayoutInflater inflater = getLayoutInflater();
+		View layout = inflater.inflate(R.layout.dialog_about, null);
+		TextView text = (TextView) layout.findViewById(R.id.about_text);
+		text.setText(Html.fromHtml(getString(R.string.message_about)));
+		builder.setView(layout);
+
+		builder.setPositiveButton(R.string.ok, null);
+		builder.show();
+	}
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
@@ -64,6 +82,9 @@ public class MainActivity extends Activity implements OnSharedPreferenceChangeLi
 		case R.id.action_new_game:
 			board.newGame();
 			board.invalidate();
+			return true;
+		case R.id.action_about:
+			about();
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
